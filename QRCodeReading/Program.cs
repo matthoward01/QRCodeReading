3f66ea3f69bbc473f8ffa86f0cf7f8951d12e532
matthoward01 <matthoward30740@gmail.com>
2023-05-18 14:08:58 -0400

    Adding Cropping

--- QRCodeReading/Program.cs
+++ QRCodeReading/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ZXing;
+using PdfToImage;
 
 namespace QRCodeReading
 {
@@ -24,7 +25,7 @@ namespace QRCodeReading
                 Directory.Delete("Temp", true);
             }
             Directory.CreateDirectory("Temp");
-            string outputFile = Path.Combine(Path.GetFileNameWithoutExtension(pdfDirectory), string.Format("{0}", "QRCodes.txt"));
+            string outputFile = Path.Combine(Path.GetFullPath(pdfDirectory), string.Format("{0}", "QRCodes.txt"));
             foreach (string f in filesList)
             {
                 if (!f.Contains(".DS_Store") && f.EndsWith(".pdf"))
@@ -33,6 +34,18 @@ namespace QRCodeReading
                 }
             }
         }
+
+        private static void JpgCreate(string input, string output, int quality, int xRes, int yRes, int firstPage, int lastPage)
+        {
+            PdfToImage.PDFConvert pp = new PDFConvert();
+            pp.OutputFormat = "jpeggray"; //format
+            pp.JPEGQuality = quality; //100% quality
+            pp.ResolutionX = xRes; //dpi
+            pp.ResolutionY = yRes;
+            pp.FirstPageToConvert = firstPage; //pages you want
+            pp.LastPageToConvert = lastPage;
+            pp.Convert(input, output);
+        }        
 
         private static void GetImages(string sourcePdf, string tempFolder, string outputFile)
         {
@@ -43,31 +56,53 @@ namespace QRCodeReading
             {
                 PdfPageBase page = doc.Pages[i];
 
-                System.Drawing.Image image = doc.SaveAsImage(0);
+                //System.Drawing.Image image = doc.SaveAsImage(0);
+                
 
                 tempFolder = Path.Combine(tempFolder, string.Format(@"{0}.jpg", Path.GetFileNameWithoutExtension(sourcePdf)));
+                JpgCreate(sourcePdf, tempFolder, 100, 1200, 1200, 1, 1);
 
-                GetQRCodeString(Path.GetFileNameWithoutExtension(sourcePdf), image, tempFolder, outputFile);
+                GetQRCodeString(Path.GetFileNameWithoutExtension(sourcePdf), tempFolder, outputFile);
  
             }
         }
-
-        private static void GetQRCodeString(string fileName, System.Drawing.Image img, string tempFolder, string outputFile)
+        public static string Crop(Bitmap b, Rectangle r, string tempFolder)
         {
-            img.Save(tempFolder, System.Drawing.Imaging.ImageFormat.Jpeg);
+            string cropFile = tempFolder;
+            using (var nb = new Bitmap(r.Width, r.Height))
+            {
+                using (Graphics g = Graphics.FromImage(nb))
+                {
+                    g.DrawImage(b, -r.X, -r.Y);
+                    cropFile = Path.Combine(Path.GetDirectoryName(tempFolder), Path.GetFileNameWithoutExtension(tempFolder) + " Crop.jpg");
+                    nb.Save(cropFile, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                
+            }
+            return cropFile;
+        }
 
+        private static void GetQRCodeString(string fileName, string tempFolder, string outputFile)
+        {
+            //img.Save(tempFolder, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            //Rectangle rectangle = new Rectangle(620, 400, 750, 300);
+            //tempFolder = Crop((Bitmap)Image.FromFile(tempFolder), rectangle, tempFolder);
             IBarcodeReader reader = new BarcodeReader();
             var barcodeBitmap = (Bitmap)Image.FromFile(tempFolder);   
-            var result = reader.Decode(barcodeBitmap);
+            var result = reader.DecodeMultiple(barcodeBitmap);
 
             if (result != null)
             {
-                string resultText = result.Text;
-                Console.WriteLine(resultText);
-                using (StreamWriter writer = new StreamWriter(outputFile, append: true))
+                foreach (var r in result)
                 {
-                    writer.WriteLine(fileName + "|" + resultText);
-                }
+                    string resultText = r.Text;
+                    Console.WriteLine(fileName + "|" + resultText);
+                    using (StreamWriter writer = new StreamWriter(outputFile, append: true))
+                    {
+                        writer.WriteLine(fileName + "|" + resultText);
+                    }
+                }                
             }
             else
             {
